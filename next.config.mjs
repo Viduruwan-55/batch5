a8521fee2deba9e445bd7c254bdc26f505206c50
Viduruwan-55 <ypviduruwan55@gmail.com>
2025-03-19 10:20:12 +0530

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,19 @@
-import nextra from 'nextra'
+import nextra from "nextra";
 
 const withNextra = nextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx',
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.jsx",
 });
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
 export default withNextra({
   // Ensure the project builds for static export
-  output: 'export',
+  output: "export",
 
   // Support for GitHub Pages
-  basePath: isProd ? '/batch5' : '',
-  assetPrefix: isProd ? '/batch5' : '',
+  basePath: isProd ? "/batch5" : "",
+  assetPrefix: isProd ? "/batch5" : "",
 
   // Required if you're using next/image
   images: {
